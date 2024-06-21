6c8a8b9a74890ac0857025db9e02dc80eecb4fbf
sowmik-sec <sowmik.sec@gmail.com>
2024-06-21 17:08:21 +0600

    dynamically fetch data from api

--- scripts.js
+++ scripts.js
@@ -20,15 +20,31 @@ async function fetchCategory() {
   //   div.classList.add();
   //   div.classList.add();
   // console.log(div);
+  let catId;
   const catDivs = catItems.map((item) => {
     const tempDiv = document.createElement("div");
     tempDiv.innerHTML = `
         <div class="px-5 py-2 text-center mb-3 bg-gray-200 rounded cursor-pointer mr-6">${item.category}</div>
     `;
+    tempDiv.addEventListener("click", () => {
+      fetchCourses(item.category_id);
+    });
     return tempDiv;
   });
   catDivs.forEach((cat) => div.appendChild(cat));
-  console.log(div);
+  //   console.log(div);
   categories.appendChild(div);
 }
 fetchCategory();
+
+async function fetchCourses(id = 1000) {
+  let courses;
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const json = await res.json();
+  courses = json.data;
+  console.log(courses);
+}
+
+fetchCourses();
